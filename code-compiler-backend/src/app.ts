f90d7503f5171bb050f73201cf5699e55d0564cf
arjun7736 <arjunvengassery123@gmail.com>
2026-08-14 12:55:48 +0530

    Add why-api-slow package and integrate middleware for performance monitoring

--- code-compiler-backend/src/app.ts
+++ code-compiler-backend/src/app.ts
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import compilerRoutes from "./routes/compilerRoutes";
 import cors from "cors"
 import prePullImages from "./utils/pre-pull-images";
+import { whySlow } from "why-api-slow";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(whySlow())
 
 app.use("/api/compiler",compilerRoutes);
 
